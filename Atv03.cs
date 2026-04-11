a5fed9814e54cc166c042a0dfa42ea54eaaec978
JMR_996 <mateusjoseph@gmail.com>
2026-04-10 21:49:36 -0300

    feat: Atv03.cs
    Inserindo enunciado da Atv03

---
+++ Atv03.cs
@@ -0,0 +1,27 @@
+/*
+Um estacionamento deseja registrar a entrada de veículos durante o dia. 
+Para cada veículo que entrar, o atendente deve informar a placa. 
+O sistema deve continuar pedindo novas placas enquanto o 
+operador desejar continuar o atendimento.
+
+Ao final, o programa deve mostrar:
+- a quantidade total de veículos registrados.
+
+Regras da atividade
+- o programa deve usar while para repetir o cadastro das placas;
+- o programa deve usar if para verificar se a placa informada está vazia;
+- se a placa estiver vazia, o sistema deve informar que o valor é inválido e não deve contar esse registro;
+- após cada tentativa de cadastro, o programa deve perguntar se o operador deseja continuar;
+- ao final, exibir a quantidade de veículos registrados com sucesso.
+
+Desafio extra
+Modifique o programa para também informar:
+-quantas tentativas inválidas aconteceram.
+*/
+public static class Atv03
+{
+    public static void Executar()
+    {
+
+    }
+}
